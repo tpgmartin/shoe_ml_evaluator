fcae95ddf0144e8f826e7cd2ce4c9bb2ad749793
tpgmartin <tpgmartin@gmail.com>
2019-06-30 01:57:18 +0100

    Get full info from browse API and save to CSV

--- scripts/parse_browse_api_responses.py
+++ scripts/parse_browse_api_responses.py
@@ -1,7 +1,11 @@
 import argparse
+import ast
 from glob import glob
 import json
 import pandas as pd
+from pandas.io.json import json_normalize
+
+# JSON downloaded 28.06.19
 
 if __name__ == "__main__":
 
@@ -12,5 +16,30 @@ if __name__ == "__main__":
             data = json.load(f)
             json_files.extend(data["Products"])
 
-    df = pd.DataFrame.from_dict(json_files, orient='columns')
+    df = pd.DataFrame.from_dict(json_files, orient="columns")
+    df["imageUrl"] = df["media"].apply(lambda media: media["imageUrl"])
+
+    cols_to_drop = [
+        "_tags",
+        "breadcrumbs",
+        "buying_countries",
+        "charityCondition",
+        "childId",
+        "countryOfManufacture",
+        "dataType",
+        "description",
+        "doppelgangers",
+        "hidden",
+        "ipoDate",
+        "lock_selling",
+        "market",
+        "media",
+        "minimumBid",
+        "productCategory",
+        "selling_countries",
+        "shoeSize",
+        "traits",
+        "type"
+    ]
+    df.drop(cols_to_drop, inplace=True, axis=1)
     df.to_csv("./data/browse_api_product_info.csv", index=False)
